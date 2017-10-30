7215d939b9d85a04c6407f0811fb267a43ef77dc
Tyler <tyler@burnamtech.com>
2017-10-30 11:58:23 -0400

    Added locked queue, pooling, and synchronization

--- rodenComputeLayer/src/APIThreads.h
+++ rodenComputeLayer/src/APIThreads.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "../util/RodenThreadLib.h"
+#include "../util/RodenLockedQueue.h"
 
 void sendAPICall();
-void print(int thread);
+void print(int thread, int item);
 void wait();
 void sendAPICallWithTimeout(int threadID);
---
+++ rodenComputeLayer/util/RodenLockedQueue.h
@@ -0,0 +1,53 @@
+#ifndef CONCURRENT_QUEUE_
+#define CONCURRENT_QUEUE_
+
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+template <typename T>
+class Queue
+{
+  public:
+    T pop()
+    {
+        std::unique_lock<std::mutex> mlock(mutex_);
+        if (queue_.empty())
+        {
+            return -1;
+        }
+        auto value = queue_.front();
+        queue_.pop();
+        return value;
+    }
+
+    void pop(T &item)
+    {
+        std::unique_lock<std::mutex> mlock(mutex_);
+        while (queue_.empty())
+        {
+            cond_.wait(mlock);
+        }
+        item = queue_.front();
+        queue_.pop();
+    }
+
+    void push(const T &item)
+    {
+        std::unique_lock<std::mutex> mlock(mutex_);
+        queue_.push(item);
+        mlock.unlock();
+        cond_.notify_one();
+    }
+    Queue() = default;
+    Queue(const Queue &) = delete;
+    Queue &operator=(const Queue &) = delete;
+
+  private:
+    std::queue<T> queue_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+};
+
+#endif

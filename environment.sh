f4e9ff88b185ef7cdaf4a7990ff46acee3765416
Ricky Taboada <rtaboada@localhost.localdomain>
2017-11-15 13:13:33 -0500

    .gitignore

--- environment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export GOOGLE_APPLICATION_CREDENTIALS=/Users/ricardotaboada/repos/rodenCluster/auth/key.json
---
+++ rodenComputeLayer/audio/environment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export GOOGLE_APPLICATION_CREDENTIALS=./key.json
